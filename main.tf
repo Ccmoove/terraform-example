2e3e010605506f8886bae8175cd5ee65ae309356
Ccmoove <152177314+Ccmoove@users.noreply.github.com>
2025-06-03 13:25:59 +0100

    Add files via upload
    IAC code to deploy configurable web server

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.xhtml
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
     user_data_replace_on_change = true
 
@@ -76,11 +76,15 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+ variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+ }
 
